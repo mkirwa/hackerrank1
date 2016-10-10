360368d00ab1c466519c390cdca2f4bf5e25a56a
mkirwa <mahlonkibiwott@gmail.com>
2016-10-09 23:34:03 -0400

    first commit

--- hackerrank1.java
+++ hackerrank1.java
@@ -72,3 +72,44 @@ public class Arithmetic {
         
     }
 }
+
+//hackerrank day 3
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+   
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt(); 
+      scan.close();
+      String ans="";
+          
+      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+      if(n%2==1){
+         System.out.println("Weird");
+      }
+      else if (n%2==0 && 2<=n && n<=5){
+          
+          System.out.println("Not Weird");
+      }
+       else if(n%2==0 && 6<=n && n<=20)
+      {
+          System.out.println("Weird"); 
+      }
+       else if(n%2==0 && n>20)
+       {
+          System.out.println("Not Weird");
+
+       }
+       else
+       {
+       
+      System.out.println(ans);
+       }
+       
+   }
+}
