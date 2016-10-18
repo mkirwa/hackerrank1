4c957ee420c7e31c34aa689102e6ff6f65b35930
mkirwa <mahlonkibiwott@gmail.com>
2016-10-17 21:50:52 -0400

    Add files via upload

--- hackerrank1.java
+++ hackerrank1.java
@@ -255,7 +255,41 @@ public class example1 {
   
 
 }
+///Day 6 hackerrank
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testCases = scan.nextInt();
+        
+        for(int i = 0; i < testCases; i++){
+            char[] inputString = scan.next().toCharArray();
+      
+            // Print even chars
+            for(int j = 0; j < inputString.length; j += 2){
+                System.out.print(inputString[j]);
+            }
+            System.out.print(" ");
+            
+            // Print odd chars
+            for(int j = 1; j < inputString.length; j += 2){
+                System.out.print(inputString[j]);
+            }
+            System.out.println();
+        }
+        scan.close();
+
+        
+        
+    }
+}
 
 
 
