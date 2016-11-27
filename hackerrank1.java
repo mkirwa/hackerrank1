4e5a7baf6c67bc546bd1da005e37ba9e33d9f644
mkirwa <mahlonkibiwott@gmail.com>
2016-11-26 23:29:23 -0500

    Add files via upload

--- hackerrank1.java
+++ hackerrank1.java
@@ -355,10 +355,107 @@ public class Solution {
     }
 }
 
-=======
-=======
->>>>>>> 4c957ee420c7e31c34aa689102e6ff6f65b35930
->>>>>>> 0b7cb263fb41a9d9712a9694aefb972e23d8f7c9
 
+
+
+
+//hacker rank day Dictionaries and maps
+
+
+import java.util.*;
+
+public class DictionaryPractise
+{
+
+    public static void main(String[]args)
+    {
+
+      //English to Spanish Dictionary
+      Map<String,String> englSwaDictionary = new HashMap<String, String>();
+      //putting things inside of a dictionary
+      englSwaDictionary.put("Monday","Jumatatu");
+      englSwaDictionary.put("Tuesday","Jumanne");
+      englSwaDictionary.put("Wednesday","Jumatano");
+      englSwaDictionary.put("Thursday","Alhamisi");
+      englSwaDictionary.put("Friday","Ijumaa");
+      //Getting things from a dictionary
+      System.out.println(englSwaDictionary.get("Monday"));
+      System.out.println(englSwaDictionary.get("Tuesday"));
+      System.out.println(englSwaDictionary.get("Wednesday"));
+      System.out.println(englSwaDictionary.get("Thursday"));
+      System.out.println(englSwaDictionary.get("Friday"));
+      //Getting the out all the keys
+      System.out.println(englSwaDictionary.keySet());
+      //Getting out all the values
+      System.out.println(englSwaDictionary.values());
+      //Getting the size of the dictionary
+      System.out.println("the size of the dictionary is "+englSwaDictionary.size());
+
+
+      //Shopping list
+
+      Map<String, Boolean> shoppingList = new HashMap<String, Boolean>();
+
+      //put items into the shopping lise
+
+      shoppingList.put("ham",true);
+      shoppingList.put("bread",Boolean.TRUE);
+      shoppingList.put("eggs",true);
+      shoppingList.put("oreos",Boolean.FALSE);
+
+      //retrive items
+
+      //key value parents
+
+      System.out.println(shoppingList.toString());
+
+      //
+      //shoppingList.clear(); ----clears the list
+      //shoppingList.remove("eggs"); ----removes the eggs
+
+      //finding out if the dictionary is empty
+
+      System.out.println(shoppingList.isEmpty());
+
+      //replacing values for a certain key
+
+      shoppingList.replace("bread",Boolean.FALSE);
+
+      System.out.println(shoppingList.toString());
+
+
+
+
+
+
+      System.out.println();
+      System.out.println();
+      System.out.println();
+      System.out.println();
+
+
+      //Hacker rank challenge question
+
+
+      //Task 
+      //Given n names and phone numbers
+      //assemble a phone book that maps friends' names to their respective phone numbers.
+      // You will then be given an unknown number of names to query your phone book for.
+      //For each name queried, print the associated entry from your phone book on a new line in the form name=phoneNumber;
+      // if an entry for name is not found, print Not found instead.
+
+
+      
+
+
+
+
+
+
+
+    }
+
+
+}
 
 
