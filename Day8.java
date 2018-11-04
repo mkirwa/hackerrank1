aced92e36c77abf06dd690971b1d5a378f9429fd
mkirwa <mahlonkibiwott@gmail.com>
2018-11-03 22:25:46 -0500

    Add files via upload

--- Day8.java
+++ Day8.java
@@ -6,11 +6,12 @@ class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         String name;
+        int phone;
         int n = in.nextInt();
         Map<String, Integer> phonebook = new HashMap<String, Integer>();
         for(int i = 0; i < n; i++){
             name = in.next();
-            int phone = in.nextInt();
+            phone = in.nextInt();
             // Write code here
             //given Given n names and phone numbers, assemble a phone book that maps friends'                 //names to their respective phone numbers. You will then be given an unknown number of             //names to query your phone book for. For each name queried, print the associated                 //entry from your phone book on a new line in the form name=phoneNumber; if an entry               //for name is not found, print Not found instead.
             phonebook.put(name,phone);
