d1b8e76b5ef037f4ae3df9b4807108be9caad0bb
mkirwa <mahlonkibiwott@gmail.com>
2018-11-03 22:22:58 -0500

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,29 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        String name;
+        int n = in.nextInt();
+        Map<String, Integer> phonebook = new HashMap<String, Integer>();
+        for(int i = 0; i < n; i++){
+            name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            //given Given n names and phone numbers, assemble a phone book that maps friends'                 //names to their respective phone numbers. You will then be given an unknown number of             //names to query your phone book for. For each name queried, print the associated                 //entry from your phone book on a new line in the form name=phoneNumber; if an entry               //for name is not found, print Not found instead.
+            phonebook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(s.equalsIgnoreCase(name)){
+                System.out.println(phonebook.toString());
+            }else{
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
