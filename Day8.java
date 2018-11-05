c16fdb7d14de92e716619816311139255c232e21
mkirwa <mahlonkibiwott@gmail.com>
2018-11-04 22:40:46 -0600

    Add files via upload

--- Day8.java
+++ Day8.java
@@ -5,41 +5,43 @@ import java.io.*;
 class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        //taking the integer from the user
-        int n = in.nextInt();
-        //taking the phone number and the name from the user
+        int n;
+        do{
+            n = in.nextInt();
+        }while(n<1 && n>Math.pow(10,5));
+     
         Map<String, Integer> phonebook = new HashMap<String, Integer>();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt(); 
-            //storing the name and the phone numbers in the newly created phonebook 
             phonebook.put(name,phone);
-            //given Given n names and phone numbers, assemble a phone book that maps friends'
-            //names to their respective phone numbers. You will then be given an unknown number of
-            //names to query your phone book for. For each name queried, print the associated
-            //entry from your phone book on a new line in the form name=phoneNumber; if an entry
-            //for name is not found, print Not found instead.
+            //given Given n names and phone numbers, assemble a phone book that maps friends'                 //names to their respective phone numbers. You will then be given an unknown number of             //names to query your phone book for. For each name queried, print the associated                 //entry from your phone book on a new line in the form name=phoneNumber; if an entry               //for name is not found, print Not found instead.
         }
-        int i = 0;
-        while(in.hasNext()){
+        
+        int count = 0;
+            while(in.hasNext()){
             String s = in.next();
             int j=0;
-            //looping through the keys and comparing them to the name entered from the user
+            // Write code here
             for(String key: phonebook.keySet()){
                 if(s.equalsIgnoreCase(key)){
                     System.out.format("%s=%d\n",s,phonebook.get(key));
-                    //if the name is found increament j 
                     j++;
                 }
             }
-            //if j is incremented, it means the name was found.
-            if(j>0){
+            
+            if(j==1){
                 continue;
             }else{
                 System.out.println("Not found");
             }
-            
+             count++;
+            if(count>Math.pow(10,5) && count <1){
+                break;
+            }
         }
+            
+        
 
 
         in.close();
