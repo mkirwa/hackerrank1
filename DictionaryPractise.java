8987ff09290e84a8359e025bb0f68357b033fdb8
mkirwa <mahlonkibiwott@gmail.com>
2018-11-05 10:03:18 -0600

    Add files via upload

--- DictionaryPractise.java
+++ DictionaryPractise.java
@@ -1,150 +1,76 @@
-
-
-//hacker rank day Dictionaries and maps
-
-
+//Dictionaries and Hashmaps in Java
+//Dictionaries don’t really exist in java
+//it’s an abstract class and so other
+//things are called. So instead we use maps.  
 import java.util.*;
+public class DictionaryPractise{
 
-public class DictionaryPractise
-{
-
-		public static void main(String[]args)
-		{
-
-			//English to Spanish Dictionary
-			Map<String,String> englSwaDictionary = new HashMap<String, String>();
-			//putting things inside of a dictionary
-			englSwaDictionary.put("Monday","Jumatatu");
-			englSwaDictionary.put("Tuesday","Jumanne");
-			englSwaDictionary.put("Wednesday","Jumatano");
-			englSwaDictionary.put("Thursday","Alhamisi");
-			englSwaDictionary.put("Friday","Ijumaa");
-			//Getting things from a dictionary
-			System.out.println(englSwaDictionary.get("Monday"));
-			System.out.println(englSwaDictionary.get("Tuesday"));
-			System.out.println(englSwaDictionary.get("Wednesday"));
-			System.out.println(englSwaDictionary.get("Thursday"));
-			System.out.println(englSwaDictionary.get("Friday"));
-			//Getting the out all the keys
-			System.out.println(englSwaDictionary.keySet());
-			//Getting out all the values
-			System.out.println(englSwaDictionary.values());
-			//Getting the size of the dictionary
-			System.out.println("the size of the dictionary is "+englSwaDictionary.size());
-
-
-			//Shopping list
-
-			Map<String, Boolean> shoppingList = new HashMap<String, Boolean>();
-
-			//put items into the shopping lise
-
-			shoppingList.put("ham",true);
-			shoppingList.put("bread",Boolean.TRUE);
-			shoppingList.put("eggs",true);
-			shoppingList.put("oreos",Boolean.FALSE);
-
-			//retrive items
-
-			//key value parents
-
-			System.out.println(shoppingList.toString());
-
-			//
-			//shoppingList.clear(); ----clears the list
-			//shoppingList.remove("eggs"); ----removes the eggs
-
-			//finding out if the dictionary is empty
-
-			System.out.println(shoppingList.isEmpty());
-
-			//replacing values for a certain key
-
-			shoppingList.replace("bread",Boolean.FALSE);
-
-			System.out.println(shoppingList.toString());
+	public static void main(String[]args){
+		//english to spanis dictionary
+		//the first string is string because string is the data type 
+		//for the key. The second string is string because the data type
+		//for the value is String 
+		Map<String, String> engspandictionary = new HashMap<String, String>();
+		//inserting keys and values into the library
+		engspandictionary.put("Monday","Lunes");
+		engspandictionary.put("Tuesday","Martes");
+		engspandictionary.put("Wednesday","Miercoles ");
+		engspandictionary.put("Thursday","Juves");
+		engspandictionary.put("Friday","Viernes");
+		//retrieving the values from the dictionary
+		System.out.println(engspandictionary.get("Monday"));
+		System.out.println(engspandictionary.get("Tuesday"));
+		System.out.println(engspandictionary.get("Wednesday"));
+		System.out.println(engspandictionary.get("Thursday"));
+		System.out.println(engspandictionary.get("Friday"));	
+		//print out all keys 
+		System.out.println(engspandictionary.keySet());	
+		//print out all values
+		System.out.println(engspandictionary.values());
+		//print out the size
+		System.out.println(engspandictionary.size());
+		//checks whether the dictionary contains a key word
+		//if it does, print that key
+		if(engspandictionary.containsKey("Monday")){
+			System.out.println(engspandictionary.get("Monday"));
+		}
 
 
-
-
-
-
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-
-
-			//Hacker rank challenge question
-
-
-			//Task 
-			//Given n names and phone numbers
-			//assemble a phone book that maps friends' names to their respective phone numbers.
-			// You will then be given an unknown number of names to query your phone book for.
-			//For each name queried, print the associated entry from your phone book on a new line in the form name=phoneNumber;
-			// if an entry for name is not found, print Not found instead.
-
-
-			
-					import java.util.*;
-					import java.io.*;
-
-					class Solution{
-					    public static void main(String []argh){
-					        Scanner in = new Scanner(System.in);
-					        int n = in.nextInt();
-					        
-					            //Task 
-								//Given n names and phone numbers
-								//assemble a phone book that maps friends' names to their respective phone numbers.
-								// You will then be given an unknown number of names to query your phone book for.
-								//For each name queried, print the associated entry from your phone book on a new line in the form     q               //name=phoneNumber;
-								// if an entry for name is not found, print Not found instead.
-
-
-					        //System.out.println(n);
-					        Map<String, Integer> phonebook = new HashMap<String, Integer>();
-					        
-					        for(int i = 0; i < n; i++){
-					            String name = in.next();
-					            int phone = in.nextInt();
-					            // Write code here
-					            
-					            phonebook.put(name,phone);
-					            //System.out.println(phonebook.toString());
-					            
-					            
-					        }
-					        
-					        while(in.hasNext()){
-					            //take in the query
-					            String name = in.next();
-					            
-					            if (!phonebook.containsKey(name)) 
-					            {
-					                System.out.println("Not found");
-					            } 
-					            else{
-					                System.out.printf("%s=%s\n", name, Integer.toString(phonebook.get(name)));
-					            }
-					                
-					            
-					        }
-					        
-					        in.close();
-					    }
-					}
+		//Create a shopping list with a boolean.
+		//do you need carrots? NO or Yes
+		Map<String, Boolean> shoppingList = new HashMap<String, Boolean>();
+		//inserting keys and values into the dictionary 
+		shoppingList.put("Ham",true);
+		//Boolean.TRUE is the Boolean object corresponding to the primitive type false
+		//it is teh same us the one above(for Ham)
+		//ofcourse we can write our primitive value
+		//in there and it will probably be fine but 
+		//if we want to be extra safe use objects inside of the map
+		//like the case below(Boolean.TRUE)
+		//Boolean is an object we have properties which is why we can
+		//do Boolean.True()....using . 
+		shoppingList.put("Bread",Boolean.TRUE);
+		shoppingList.put("Eggs", Boolean.FALSE);
+		shoppingList.put("Sugar",false);
+		//retrieving items
+		System.out.println(shoppingList.get("Ham"));
+		//printing key-value pair
+		System.out.println(shoppingList.toString());
+		//clearing the dictionary
+		shoppingList.clear();
+		//checking whether the dictionaries are empty
+		//Is empty? Is it empty? prints True if it's empty
+		System.out.println(shoppingList.isEmpty());
+		//You can also removed either an object or a key
+		shoppingList.remove("Eggs");
+		//printing key-value pair to see if the Eggs were removed
+		System.out.println(shoppingList.toString());
+		//replacing things 
+		shoppingList.replace("Bread",Boolean.FALSE);
+		//printing key-value pair to see if the Bread was replaced
+		System.out.println(shoppingList.toString());
 
 
 
-
-
-
-
-
-
-							}
-
-
-					}
+	}
+}
